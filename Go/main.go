a5a260230116ad6c2b3d4b2ca13088230b1c21f9
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2025-01-21 14:28:05 -0500

    Complete Go version

---
+++ Go/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	message := "Hello, World!" // Short hand for below, preferred method...
+	// message: string = "Hello, World!"
+	fmt.Println(message)
+}
